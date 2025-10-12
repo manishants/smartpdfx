b46dd52b5a9b5a23cb072e7e9289798f4214988f
KathaLok <kathaloknishant@gmail.com>
2025-10-12 20:41:09 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -24,7 +24,7 @@ export function createClient() {
         },
         remove(name: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, '', ...options })
+            cookieStore.set(name, '', options)
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
