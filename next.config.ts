347c0dc9fe30964620bb2520975622ace3f97570
KathaLok <kathaloknishant@gmail.com>
2025-10-12 20:59:13 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch. Error sou

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
 };
 
 export default nextConfig;
